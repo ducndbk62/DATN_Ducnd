1a303b957f30378cdf944313e3e36ce02d6fb234
ducndbk62 <ducndbk62@gmail.com>
2022-07-06 01:52:28 +0700

    first commit

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject UI_MainMenu;
+
+    public GameObject UI_Setting;
+    public GameObject UI_About;
+    public GameObject UI_SelectMode;
+    public Slider slider;
+
+    public void OpenSelectMode()
+    {
+        UI_MainMenu.SetActive(false);
+        UI_SelectMode.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Fight()
+    {
+        SceneManager.LoadScene("FightMode");
+
+    }
+
+    public void Practice()
+    {
+        SceneManager.LoadScene("PracticeMode");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        UI_About.SetActive(false);
+        UI_Setting.SetActive(false);
+        UI_SelectMode.SetActive(false);
+
+        UI_MainMenu.SetActive(true);
+    }
+
+    public void OpenSetting()
+    {
+        UI_MainMenu.SetActive(false);
+        UI_Setting.SetActive(true);
+    }
+
+    public void OpenAbout()
+    {
+        UI_MainMenu.SetActive(false);
+        UI_About.SetActive(true);
+    }
+
+    public void SettingAudio()
+    {
+        AudioListener.volume = slider.value;
+    }
+}
